eabd62343a15d38ddfbd794caf2ebca8546efd3f
Vladlens Podnebess <vladpod@inbox.lv>
2024-03-26 09:53:43 +0200

    removed overflow from choice page

--- login-register.js
+++ login-register.js
@@ -14,8 +14,30 @@ loginLink.addEventListener('click', ()=> {
 
 btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
+    removeActivity();
 });
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+    addActivity();
 });
+
+function removeActivity() 
+{
+    document.getElementById('container1').style.pointerEvents = "none";
+    document.getElementById('container2').style.pointerEvents = "none";
+    document.getElementById('container3').style.pointerEvents = "none";
+    document.getElementById('container4').style.pointerEvents = "none";
+    document.getElementById('container5').style.pointerEvents = "none";
+    document.getElementById('container6').style.pointerEvents = "none";
+}
+
+function addActivity()
+{
+    document.getElementById('container1').style.pointerEvents = "auto";
+    document.getElementById('container2').style.pointerEvents = "auto";
+    document.getElementById('container3').style.pointerEvents = "auto";
+    document.getElementById('container4').style.pointerEvents = "auto";
+    document.getElementById('container5').style.pointerEvents = "auto";
+    document.getElementById('container6').style.pointerEvents = "auto";
+}
